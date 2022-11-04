1a0db47677a987164f6b9283fb8a1d9d23dcdd66
“Nirmal <nirmalb@dreamworld.dev>
2022-11-04 16:16:22 +0530

    feat: show ellipsisi & tooltip when text overflows.
    card link: https://a.kerika.net/C7r/board/Zkwh/Zsme

--- dw-ellipsis.js
+++ dw-ellipsis.js
@@ -1,32 +1,44 @@
-import { LitElement, html, css } from '@dreamworld/pwa-helpers/lit.js';
+import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
 
+/**
+ * Purpose: Shows ellipsis & full text in tooltip when it overflows.
+ *
+ * Behaviors:
+ *  - Applies ellipsis style when text overflows.
+ *  - When user hovers on text & its overflowed, shows it in tooltip.
+ * 
+ * Usage pattern:
+ *  <dw-ellipsis>Your text here.</dw-ellipsis>
+ */
 export class DwEllipsis extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * When ellipsis is applied, shows tooltip on hover.
-       */
-      _title: { type: String, reflect: true, attribute: 'title' }
-    }
-  }
+  static styles = [css``];
 
-  firstUpdated(changedProps) {
-    super.firstUpdated && super.firstUpdated(changedProps);
-    this.__computeEllipsis();
+  static properties = {
+    /**
+     * When ellipsis is applied, shows tooltip on hover.
+     */
+    _toolTipText: { type: String },
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener("mouseenter", this._showTooltip);
+    this.addEventListener("mouseleave", this._hideTooltip);
   }
 
   render() {
-    return html`
-      <slot></slot>
-    `;
+    return html` <slot></slot> `;
   }
 
   /**
-   * When text overflows, sets title of element.
+   * When text overflows, shows text content in tooltip.
    */
-  __setTitle() {
-    this._title = '';
-  }
+  _showTooltip() {}
+
+  /**
+   * When tooltip text is set, hides tooltip.
+   */
+  _hideTooltip() {}
 }
 
-customElements.define('dw-ellipsis', DwEllipsis);
+customElements.define("dw-ellipsis", DwEllipsis);
