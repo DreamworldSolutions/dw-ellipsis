7ef3e2379aa2fb89d0f9d97cb081604d65430b45
Vaidehi Kheni <vaidehi@hisab.co>
2022-12-14 12:23:35 +0530

    feat: trigger

--- demo/demo-element.js
+++ demo/demo-element.js
@@ -1,27 +1,29 @@
-import { LitElement, html, css } from '@dreamworld/pwa-helpers/lit.js';
-import '../dw-ellipsis';
+import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
+import "../dw-ellipsis";
 
 class DemoElement extends LitElement {
   static get styles() {
     return [
       css`
-        :host{
-          display:block;
+        :host {
+          display: block;
         }
 
-        .first { 
+        .first {
           width: 50px;
         }
-      `
-    ]
+      `,
+    ];
   }
   render() {
     return html`
       <h2>dw-ellipsis Demo:</h2>
-      <dw-ellipsis class="first" .placement=${"bottom"}><strong>Hello World. Hello World</strong></dw-ellipsis><br>
+      <dw-ellipsis class="first" .placement=${"bottom"}
+        ><strong>Hello World. Hello World</strong></dw-ellipsis
+      ><br />
       <dw-ellipsis><strong>Hello World. Hello World</strong></dw-ellipsis>
-    `
+    `;
   }
 }
 
-customElements.define('demo-element', DemoElement);
+customElements.define("demo-element", DemoElement);
